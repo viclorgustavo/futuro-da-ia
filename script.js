306d61301aae2da57fbcef6a8e656cbd94b3e7b7
victor <111356623+viclorgustavo@users.noreply.github.com>
2024-06-13 09:22:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultados = document.querySelector(".caixa-resultados");
+const textoResultado = document.querySelector(".textoresultado");
+
+const perguntas = [
+    esnunciado="Pergunta 1",
+    alternativas=["alternativa1","alternativa2"]
+
+    "Pergunta 2"
+];
+
